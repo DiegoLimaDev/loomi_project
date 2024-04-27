621ff57f864346d6d7fc1a837fafa4f806ed1d5a
Diego Lima <xxdiegolsxx@gmail.com>
2024-04-27 16:14:25 -0300

    feat: :sparkles: mailVerification
    mailVerificationRoute

--- src/app/api/auth/auth.controller.ts
+++ src/app/api/auth/auth.controller.ts
@@ -1,9 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AdminService } from 'src/app/infra/admin/admin.service';
 import { AuthService } from 'src/app/infra/auth/auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private adminService: AdminService,
+  ) {}
+
+  @Get('emailVerification/:token/:email')
+  async emailVerification(
+    @Param('token') token: string,
+    @Param('email') email: string,
+  ): Promise<string> {
+    const user = await this.adminService.getOneByEmail(email);
+    await this.authService.emailVerification(token, user);
+
+    return `<h1 style="padding:10px;align-text:center;">Conta verificada com sucesso</h1>`;
+  }
 
   @Post('login')
   async login(

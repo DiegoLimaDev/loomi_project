2b670d8ac67b0e39b252e3d42314a888f47256e6
Diego Lima <xxdiegolsxx@gmail.com>
2024-04-27 16:15:28 -0300

    feat: :sparkles: api
    new routes api admin

--- src/app/api/admin/admin.controller.ts
+++ src/app/api/admin/admin.controller.ts
@@ -4,44 +4,72 @@ import {
   Delete,
   Get,
   Param,
+  Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
 import { AdminDomain } from 'src/app/entities/admin/admin.domain';
 import { AdminService } from 'src/app/infra/admin/admin.service';
 import { JwtAuthGuard } from 'src/app/infra/auth/guards/jwt.guard';
+import { MailerService } from 'src/app/infra/mailer/mailer.service';
+import { TokenGenerationService } from 'src/app/infra/token-generation/token-generation.service';
 import { IAdmin } from 'src/app/interfaces/admin/admin.interface';
 import { Usertype } from 'src/app/interfaces/shared/user/user.interface';
 
 @Controller('admin')
-@UseGuards(JwtAuthGuard)
 export class AdminController implements IAdmin {
-  constructor(private adminService: AdminService) {}
+  constructor(
+    private adminService: AdminService,
+    private tokenGeneration: TokenGenerationService,
+    private mailerService: MailerService,
+  ) {}
 
   @Post('create')
-  async create(@Body() req: AdminDomain): Promise<AdminDomain> {
+  async create(@Body() admin: AdminDomain): Promise<AdminDomain> {
+    const token = await this.tokenGeneration.generateVerificationToken(
+      admin.email,
+    );
+    await this.mailerService.sendVerificationMail(admin.email, token);
+
     return await this.adminService.create({
-      ...req,
+      ...admin,
       usertype: Usertype.Admin,
+      isVerified: false,
+      token: token,
     });
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   async getOne(@Param('id') id: number): Promise<AdminDomain> {
     return await this.adminService.getOne(id);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async getAll(): Promise<AdminDomain[]> {
     return await this.adminService.getAll();
   }
 
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  async edit(
+    @Param('id') id: number,
+    @Body() admin: AdminDomain,
+  ): Promise<boolean> {
+    await this.adminService.edit(id, admin);
+
+    return true;
+  }
+
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   async delete(@Param('id') id: number): Promise<boolean> {
     return await this.adminService.delete(id);
   }
 
   @Get('email')
+  @UseGuards(JwtAuthGuard)
   async getOneByEmail(@Param('email') email: string): Promise<AdminDomain> {
     return await this.adminService.getOneByEmail(email);
   }

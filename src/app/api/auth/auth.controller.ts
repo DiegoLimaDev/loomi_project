5cb2ccc97bf7f1f8f96f6e0ecafd9b805cdd4222
Diego Lima <xxdiegolsxx@gmail.com>
2024-04-28 19:01:33 -0300

    refactor: :recycle: injection
    changed injection method

--- src/app/api/auth/auth.controller.ts
+++ src/app/api/auth/auth.controller.ts
@@ -1,20 +1,17 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AuthService } from 'src/app/infra/auth/auth.service';
-import { UserService } from 'src/app/infra/user/user.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private authService: AuthService,
-    private userService: UserService,
-  ) {}
+  constructor(private authService: AuthService) {}
 
   @Get('emailVerification/:token/:email')
   async emailVerification(
     @Param('token') token: string,
     @Param('email') email: string,
   ): Promise<string> {
-    const user = await this.userService.getOneByEmail(email);
+    const user = await this.authService.userService.getOneByEmail(email);
+
     await this.authService.emailVerification(token, user);
 
     return `<h1 style="padding:10px;align-text:center;">Conta verificada com sucesso</h1>`;

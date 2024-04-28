7e16d11ac585c36a3e314ee0249a33aeb20f7b6d
Diego Lima <xxdiegolsxx@gmail.com>
2024-04-28 19:02:11 -0300

    feat: :sparkles: api
    client api

--- src/app/api/client/client.controller.ts
+++ src/app/api/client/client.controller.ts
@@ -1,7 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import { ClientDomain } from 'src/app/entities/client/client.domain';
+import { UserDomain } from 'src/app/entities/user/user.domain';
+import { JwtAuthGuard } from 'src/app/infra/auth/guards/jwt.guard';
 import { ClientService } from 'src/app/infra/client/client.service';
+import { IClientService } from 'src/app/interfaces/client/client.interface';
 
 @Controller('client')
-export class ClientController {
+@UseGuards(JwtAuthGuard)
+export class ClientController implements IClientService {
   constructor(private clientService: ClientService) {}
+
+  @Get(':id')
+  async getOne(@Param('id') id: number): Promise<ClientDomain> {
+    return await this.clientService.getOne(id);
+  }
+
+  @Get()
+  async getAll(): Promise<ClientDomain[]> {
+    return await this.clientService.getAll();
+  }
+
+  @Patch(':id')
+  async edit(
+    @Param(':id') id: number,
+    @Body() client: ClientDomain,
+  ): Promise<{ deleted: boolean }> {
+    return await this.clientService.edit(id, client);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<{ deleted: boolean }> {
+    return await this.clientService.delete(id);
+  }
+
+  @Delete(':id')
+  async deleteLogical(id: number): Promise<{ deleted: boolean }> {
+    return await this.clientService.deleteLogical(id);
+  }
+
+  @Get('byUser')
+  async getOneByFkUserId(@Body() user: UserDomain): Promise<ClientDomain> {
+    return await this.clientService.getOneByFkUserId(user);
+  }
 }

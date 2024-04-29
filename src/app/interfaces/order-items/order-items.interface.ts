38c821d9d1b2855ab8f5483533205d3de2763a62
Diego Lima <xxdiegolsxx@gmail.com>
2024-04-29 17:41:23 -0300

    feat: :sparkles: api
    get all itens in the order

--- src/app/interfaces/order-items/order-items.interface.ts
+++ src/app/interfaces/order-items/order-items.interface.ts
@@ -2,6 +2,8 @@ import { OrderItemsDomain } from 'src/app/entities/order-items/order-items.domai
 
 export interface IOrderItems {
   create(orderData: OrderItemDto): Promise<OrderItemsDomain>;
+
+  getItemsInOrder(orderId: number): Promise<OrderItemsDomain[]>;
 }
 
 export type OrderItemDto = {

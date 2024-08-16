c95c034b1583164d12fd5f9714c757ec1a500535
Diego Lima <xxdiegolsxx@gmail.com>
2024-08-16 12:01:20 -0300

    feat: :sparkles: api
    added api tags for swagger

--- src/app/api/redis/redis.controller.ts
+++ src/app/api/redis/redis.controller.ts
@@ -1,16 +1,32 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { redisDto } from 'src/app/dto/redis/redis.dto';
+import { JwtAuthGuard } from 'src/app/infra/auth/guards/jwt.guard';
 import { RedisService } from 'src/app/infra/redis/redis.service';
 
+@ApiBearerAuth()
+@ApiTags('redis')
 @Controller('cache')
+@UseGuards(JwtAuthGuard)
 export class RedisController {
   constructor(private readonly redisService: RedisService) {}
 
+  @ApiBody({ type: redisDto })
+  @ApiOperation({ description: 'Envia uma chave valor ao redis' })
   @Post('set')
   async setCache(@Body() data: { key: string; value: string }) {
     await this.redisService.set(data.key, data.value);
     return { success: true };
   }
 
+  @ApiParam({ name: 'key', example: '1' })
+  @ApiOperation({ description: 'get de um valor no redis' })
   @Get('get/:key')
   async getCache(@Param('key') key: string) {
     const value = await this.redisService.get(key);
